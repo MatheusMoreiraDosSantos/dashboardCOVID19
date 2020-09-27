94f3c054963670fafd9ab242db1519aad16e0bed
MatheusMoreiraDosSantos <matheusmoreira.santos699@gmail.com>
2020-09-27 16:15:13 -0300

    formata a data

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,19 +1,6 @@
-// import { zonedTimeToUtc} from 'date-fns-tz';
-// import {format} from 'date-fns';
-
 export default function formatDate(date){
     if(date){
-        // date = '2020-02-27T00:00:00Z';
-        // let pattern = "dd 'de' MMMM 'de' yyyy";
-        
-        // let dateZone = zonedTimeToUtc(date, 'America/Sao_Paulo');
-
-        // console.log(dateZone);
-
-        // let dateFormated = format(dateZone, pattern, { timeZone: 'America/Sao_Paulo' });
-
-        // console.log(format(date, pattern));
-
-        return date;
+        let new_date = date.split("T");
+        return new_date[0];
       }
 }
